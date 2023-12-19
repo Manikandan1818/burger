ff862bf08d60c1712c8a25c153dac96c7e1b6d79
Manikandan1818 <simbumanii18@gmail.com>
2023-12-19 13:48:06 +0530

    Burger app created!

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="text-4xl bg-red-700 text-white py-4 w-96">Burger APp</h1>
+  );
+};
+
+export default App;
---
+++ src/assets/images/index.js
@@ -0,0 +1,8 @@
+import about from "./about.jpeg";
+import beverage1 from "./beverage-1.png";
+import beverage2 from "./beverage-2.png";
+import beverage3 from "./beverage-3.png";
+import beverage4 from "./beverage-4.png";
+import beverage5 from "./beverage-5.png";
+
+export { about, beverage1, beverage2, beverage3, beverage4, beverage5 };
