5137ed7b1b4e91d3afcc59d02069198781a37380
Manikandan1818 <simbumanii18@gmail.com>
2023-12-22 21:14:14 +0530

    menu Filter function added

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,30 @@
+import { useState } from "react";
+import { menu } from "../constants";
 import MenuButton from "./MenuButton";
 
 const Menu = () => {
+  const [selectedMenu, setSelectedMenu] = useState("");
+
+  const handleClick = (e) => {
+    setSelectedMenu(e.target.value);
+  };
+
+  const filteredData = (menu, selectedMenu) => {
+    if (
+      selectedMenu === "Food" ||
+      selectedMenu === "Snacks" ||
+      selectedMenu === "Beverage"
+    ) {
+      return menu.filter(
+        (item) =>
+          item.category.toLocaleLowerCase() === selectedMenu.toLocaleLowerCase()
+      );
+    }
+    return menu;
+  };
+
+  const result = filteredData(menu, selectedMenu);
+
   return (
     <section id="menu" className="container">
       <h2 className="sec-title">Our Best Menu</h2>
@@ -9,11 +33,27 @@ const Menu = () => {
         The original HOME OF THE WHOPPER, our commitment to ingredients,
         signature recipes, and end-to-end online Burger.
       </p>
-      <div className="mt-5 flex flex-wrap justify-center gap-5">
-        <MenuButton label="All" active />
-        <MenuButton label="Food" />
-        <MenuButton label="Snacks" />
-        <MenuButton label="Beverage" />
+      <div className="mt-8 mb-8 flex flex-wrap justify-center gap-5">
+        <MenuButton value="All" handleClick={handleClick} />
+        <MenuButton value="Food" handleClick={handleClick} />
+        <MenuButton value="Snacks" handleClick={handleClick} />
+        <MenuButton value="Beverage" handleClick={handleClick} />
+      </div>
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4 md:gap-10">
+        {result.map((item) => (
+          <div key={item.id}>
+            <div className="h-56 grid place-items-center rounded-3xl bg-primaryColorLight hover:bg-secondaryColor ease-linear duration-200 lg:h-40">
+              <img
+                src={item.img}
+                alt="menu"
+                className="py-1 w-36 hover:scale-110 ease-linear duration-200 md:w-35 lg:w-24"
+              />
+            </div>
+            <h4 className="pt-5 title">{item.title}</h4>
+            <p className="para mb-2">{item.desc}</p>
+            <p className="text-secondaryColor">{item.price}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
--- src/components/MenuButton.jsx
+++ src/components/MenuButton.jsx
@@ -1,15 +1,17 @@
-const MenuButton = ({ label, active }) => {
+const MenuButton = ({ value, active, handleClick }) => {
   return (
-    <div
+    <button
       className={`inline-block text-sm py-2 px-6 text-center cursor-pointer rounded-lg ease-linear duration-200  ${
         active
           ? `text-black bg-secondaryColor`
           : ` text-white bg-primaryColorLight`
       }
       hover:opacity-75`}
+      value={value}
+      onClick={handleClick}
     >
-      {label}
-    </div>
+      {value}
+    </button>
   );
 };
 
